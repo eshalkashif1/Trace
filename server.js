f212f16d552d40c4b11725fa071522a15f630b9f
sanvikaushik <sanvi.kaushik22@gmail.com>
2025-10-05 12:00:08 -0400

    fix deployment

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import sqlite3 from "sqlite3";
-import fetch from "node-fetch";
 
 const app = express();
 const db = new sqlite3.Database("./database.db");
